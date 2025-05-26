9b3ed2615230a646ca7541b57a5330ef5fadc943
LorenzoBondan <lbbondan@gmail.com>
2025-05-26 09:22:23 -0300

    create and configure eureka-server

---
+++ eureka-service/src/test/java/com/projects/eurekaservice/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
