14188f3acab56ea3bb5542de848d9806e966c6c6
LorenzoBondan <lbbondan@gmail.com>
2025-05-26 14:18:20 -0300

    create and configure hr-user

---
+++ hr-user/src/test/java/com/projects/hruser/HrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.hruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
