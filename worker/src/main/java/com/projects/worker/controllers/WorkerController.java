fc7665283daedc9676990a5d92301477fc035a8d
LorenzoBondan <lbbondan@gmail.com>
2025-05-27 11:26:47 -0300

    set worker as a resource-server

--- worker/src/main/java/com/projects/worker/controllers/WorkerController.java
+++ worker/src/main/java/com/projects/worker/controllers/WorkerController.java
@@ -5,6 +5,7 @@ import com.projects.worker.repositories.WorkerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,12 +21,14 @@ public class WorkerController {
     @Autowired
     private WorkerRepository repository;
 
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
     @GetMapping
     public ResponseEntity<List<Worker>> findAll() {
         List<Worker> workers = repository.findAll();
         return ResponseEntity.ok().body(workers);
     }
 
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_CLIENT')")
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id) {
         return ResponseEntity.ok(repository.findById(id).orElse(null));
