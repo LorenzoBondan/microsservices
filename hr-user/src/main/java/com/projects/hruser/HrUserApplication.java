d5669ff4eace17a0bed4d7d0ca77a7d0f078b3e8
LorenzoBondan <lbbondan@gmail.com>
2025-05-27 14:19:33 -0300

    add hr-exception-handler and resource-server to hr-user

--- hr-user/src/main/java/com/projects/hruser/HrUserApplication.java
+++ hr-user/src/main/java/com/projects/hruser/HrUserApplication.java
@@ -2,7 +2,9 @@ package com.projects.hruser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(value = {"com.projects"})
 @SpringBootApplication
 public class HrUserApplication {
 
--- hr-user/src/main/java/com/projects/hruser/controllers/UserController.java
+++ hr-user/src/main/java/com/projects/hruser/controllers/UserController.java
@@ -1,9 +1,11 @@
 package com.projects.hruser.controllers;
 
+import com.projects.hrexceptionhandler.exceptions.ResourceNotFoundException;
 import com.projects.hruser.entities.User;
 import com.projects.hruser.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,12 +15,14 @@ public class UserController {
 
     private final UserRepository repository;
 
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_OPERATOR')")
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Long id) {
-        User obj = repository.findById(id).get();
+        User obj = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("User with id: " + id + " not found"));
         return ResponseEntity.ok(obj);
     }
 
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_OPERATOR')")
     @GetMapping(value = "/search")
     public ResponseEntity<User> findByEmail(@RequestParam String email) {
         User obj = repository.findByEmail(email);
