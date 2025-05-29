cb1ba4dd40b02933e31f1b7b7dd66a7fc9f0c63b
LorenzoBondan <lbbondan@gmail.com>
2025-05-29 14:12:15 -0300

    update notification-service, now its endpoints are secured

--- notification/src/main/java/com/projects/notification/controllers/NotificationController.java
+++ notification/src/main/java/com/projects/notification/controllers/NotificationController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,7 @@ public class NotificationController {
 
     private final NotificationService service;
 
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_OPERATOR')")
     @GetMapping(value = "/user/{id}")
     public ResponseEntity<Page<Notification>> findAllByUserId(@PathVariable("id") Long userId, Pageable pageable) {
         return ResponseEntity.ok(service.findAllByUserId(userId, pageable));
